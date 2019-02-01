d9d3225125c573e3437de4e362a9ab65be6bc2ca
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-02-01 00:45:39 -0200

    adicionando components e frameworks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mercurio'`, () => {
+  it(`should have as title 'testec'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mercurio');
+    expect(app.title).toEqual('testec');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mercurio!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testec!');
   });
 });
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mrc-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
